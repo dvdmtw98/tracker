b2e857ef3942c0200047eed59542af7714c8efb3
David Varghese <dvdmtw99@gmail.com>
2025-01-12 20:33:31 -0600

    Vault Backup: 2025-01-12 20:33

--- config/quartz/components/Footer.tsx
+++ config/quartz/components/Footer.tsx
@@ -14,7 +14,7 @@ export default ((opts?: Options) => {
     return (
       <footer class={`${displayClass ?? ""}`}>
         <p>
-          © 2014-{year} David Varghese. All rights reserved.
+          © 2024-{year} David Varghese. All rights reserved.<br />
           {" "}{i18n(cfg.locale).components.footer.createdWith}{" "}
           <a href="https://quartz.jzhao.xyz/" target="_blank" rel="noreferrer noopener">
             Quartz v{version}
